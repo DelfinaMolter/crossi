aef768f5c59c4cea6f93ba41c69824bd82b1949b
DelfinaMolter <delfi.molter@gmail.com>
2023-12-21 12:24:02 -0300

    Whatsapp

--- app/components/whatsapp/whatsapp.jsx
+++ app/components/whatsapp/whatsapp.jsx
@@ -1,9 +1,31 @@
+"use client";
+import React from "react";
 import Link from "next/link";
+import WhatsAppImg from "../../../public/whatsapp.svg"
+import Image from "next/image";
 
 
 
 export default function WhatsApp({ number }) {
+    const [whatsapp, setWhatsapp] = React.useState("");
+
+    React.useEffect(() => {
+        if (screen.width < 768) {
+            setWhatsapp(`wa.me/${number}?`);
+        } else {
+            setWhatsapp(
+                `web.whatsapp.com/send?phone=${number}&`,
+            );
+        }
+    }, []);
+
     return (
-        <Link href="#">{number}</Link>
+        <Link
+        href={`https://${whatsapp}text=Hola%20Team%20Crossi,%20necesito%20hacerles%20una%20consulta...`}
+        target="_blank"
+        >
+            <Image src={WhatsAppImg} width={"30px"} height={"30px"} alt="WhatsApp"/>
+            {number}
+        </Link>
     )
 }
